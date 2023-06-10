02a54cd6dbad74546ae5f3542aebfc113f500739
Nithin Muthukumar <kathrikai30201094@gmail.com>
2023-06-10 18:24:08 -0400

    insulter

---
+++ examples/basic.rs
@@ -0,0 +1,12 @@
+use shrs::ShellBuilder;
+use shrs_insulter::InsulterPlugin;
+
+fn main() {
+    let myshell = ShellBuilder::default()
+        .with_plugin(MuxPlugin)
+        .with_plugin(InsulterPlugin::new(vec![], 1., true))
+        .build()
+        .unwrap();
+
+    myshell.run();
+}
